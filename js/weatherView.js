ed55e9844fad74487936349344fb2ec6648c6606
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-05-27 11:30:00 +0300

    v1.0.8stable added local time

--- js/weatherView.js
+++ js/weatherView.js
@@ -7,21 +7,17 @@ import { mainScreenTemp, mainScreenWeatherDescription, mainScreenWeatherIcon, ma
 currentWeather('Tokio')
     .then(data => {
         console.log(data.data[0])
-        console.log(data.data[0].temp)
         mainScreenTemp.innerHTML = Math.round(data.data[0].temp) + '<sup>0</sup>'
-        console.log(data.data[0].weather.description)
-        console.log(mainScreenWeatherDescription)
         mainScreenWeatherDescription.innerHTML = data.data[0].weather.description
         mainScreenWeatherIcon.innerHTML = `<img src=../src/img/weather-icons/${data.data[0].weather.icon}.png width="140px">`
-        console.log(currentLocalTime('Asia/Tokyo').then())
-        mainScreenTime.innerHTML = `${currentLocalTime('Asia/Tokyo').then(data => data.datetime.slice(11,16))}`
+        currentLocalTime(data.data[0].timezone)
+            .then(data => {
+                mainScreenTime.innerHTML = data.datetime.slice(11,16)
+            })
+            .catch(alert)
+    
     })
     .catch(alert)
     
-// currentLocalTime('America/Mexico_City')
-//     .then(data => {
-//         console.log(data.datetime.slice(11,16))
-//         return data.datetime.slice(11,16)
-//     })
-//     .catch(alert)
+
 
